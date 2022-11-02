1a83c438a8dbfb020cf8628278916bae6386e936
ElyasAguiar <elyas.santana98@academico.ifs.edu.br>
2022-11-02 02:24:28 -0300

    config: configure swagger

--- start_point/api/v1.py
+++ start_point/api/v1.py
@@ -1,8 +1,14 @@
 from fastapi import APIRouter
+from starlette.responses import RedirectResponse
 
 router = APIRouter()
 
 
-@router.get("/start_point/")
+@router.get("/", include_in_schema=False)
+def get_swagger():
+    return RedirectResponse(url="/docs")
+
+
+@router.get("/start_point/health", name="health")
 def get_start_point():
     return {"health": "check!"}
